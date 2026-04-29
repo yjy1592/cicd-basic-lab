ad72f013f72a1385b123bcbcb875afb8274167ab
jun <yjy1592@gmail.com>
2026-04-29 14:08:57 +0900

    update /api/ci-cd

--- cicd-basic-lab/src/main/java/cicd/controller/CiCdController.java
+++ cicd-basic-lab/src/main/java/cicd/controller/CiCdController.java
@@ -14,6 +14,6 @@ public class CiCdController {
 
 	@GetMapping("/ci-cd")
 	public Map<String, String> getCiCd() {
-	    return Map.of("app", "ci-cd", "version", "v5");
+	    return Map.of("app", "ci-cd", "version", "v6");
 	}
 }
